f98956780233d1bc14efe7cc37c8547c6b646e2a
SAMADI74 <64611751+SAMADI74@users.noreply.github.com>
2021-11-01 16:29:23 +0100

    Initial commit

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+SECRET_KEY = os.urandom(32)
+# Grabs the folder where the script runs.
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Enable debug mode.
+DEBUG = True
+
+# Connect to the database
+
+
+# TODO IMPLEMENT DATABASE URL
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:SENATOR_74@localhost:5432/booking'
